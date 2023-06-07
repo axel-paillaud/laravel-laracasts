35122bfc8170a0cc5464f0d57f07d7047d40d3f3
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-07 19:23:33 +0200

    work with blade layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-<body>
-    <?php foreach ($posts as $post) : ?>
-    <article>
+@extends ('layout')
+
+@section('content')
+    @foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'foobar' : '' }}">
         <h1>
-            <a href="/posts/<?= $post->slug ?>">
-                <?= $post->title ?>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
             </a>
         </h1>
         <div>
-            <?= $post->excerpt ?>
+            {{ $post->excerpt}}
         </div>
     </article>
-    <?php endforeach; ?>
-</body>
-</html>
+    @endforeach
+@endsection
