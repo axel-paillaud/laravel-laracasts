6798e2a06c1383394136349515e4c1d438952657
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-10 08:28:39 +0200

    integrate POST to blade engine

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,9 +6,9 @@
 
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-    <x-post-featured-card />
+    <x-post-featured-card :post="$posts->first()" />
 
-    <div class="lg:grid lg:grid-cols-2">
+{{--     <div class="lg:grid lg:grid-cols-2">
         <x-post-card />
         <x-post-card />
     </div>
@@ -17,7 +17,7 @@
         <x-post-card />
         <x-post-card />
         <x-post-card />
-    </div>
+    </div> --}}
 </main>
 @endsection
 {{-- @foreach ($posts as $post)
