5c14b299eb941c798d2273aec0735d7ae09ba83c
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-16 16:51:55 +0200

    move logic from route to controller. move sql query from controller to model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,15 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query)
+    {
+        if (request('search')) {
+            $query
+                ->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+    }
+
     public function category()
     {
         // hasOne, hasMany, belongsTo, belongsToMany
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
@@ -17,21 +18,9 @@ use App\Models\User;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all(),
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function(Post $post) {
-    return view('post', [
-        'post' => $post,
-        'categories' => Category::all(),
-    ]);
-
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', [
