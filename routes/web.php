2f1ec71fa7f25eb6267cafa811fcb79de5978e9a
axel-paillaud <contact@axelweb.fr>
2023-06-30 19:14:07 +0200

    filter post by author : use only controller and model, we can now stack on filter

--- routes/web.php
+++ routes/web.php
@@ -7,23 +7,6 @@ use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('authors/{author:username}', function(User $author) {
-    return view('posts.index', [
-        'posts' => $author->posts
-    ]);
-});
