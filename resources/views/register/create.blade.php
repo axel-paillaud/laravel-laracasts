99d7e9f4af81dfb07251df23d59010712a4a5c14
axel-paillaud <contact@axelweb.fr>
2023-07-18 08:40:58 +0200

    failed validation and old input data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -5,6 +5,7 @@
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
             <h1 class="text-center font-bold text-xl">Register!</h1>
             <form method="post" action="/register" class="mt-10">
+                @csrf
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -16,8 +17,13 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name') }}"
                         required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -30,8 +36,54 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username') }}"
                         required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                        for="email"
+                    >
+                        Email
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                        type="email"
+                        name="email"
+                        id="email"
+                        value="{{ old('email') }}"
+                        required
+                    >
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                        for="password"
+                    >
+                        Password
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                        type="password"
+                        name="password"
+                        id="password"
+                        required
+                    >
+                </div>
+
+                <div class="mb-6">
+                    <button class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500" 
+                        type="submit"
+                    >
+                        Submit
+                    </button>
                 </div>
 
             </form>
