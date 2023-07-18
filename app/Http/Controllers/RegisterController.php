cfa68114cc5086eee8e2369cf84ec8c21115b2f0
axel-paillaud <contact@axelweb.fr>
2023-07-18 08:43:57 +0200

    make username and email Unique

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,8 +16,8 @@ class RegisterController extends Controller
     {
         $attributes = request()->validate([
             'name' => ['required', 'max:255'],
-            'username' => ['required', 'max:255', 'min:3'],
-            'email' => ['required', 'email'],
+            'username' => ['required', 'max:255', 'min:3', 'unique:users,username'],
+            'email' => ['required', 'email', 'unique:users,email'],
             'password' => ['required', 'min:7', 'max:255'],
         ]);
         
